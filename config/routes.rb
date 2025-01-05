ec89f23f52d49b0fef5370165a247f4e2945eb80
Apclassic <brodeybriggs@hotmail.com>
2025-01-05 14:21:50 -0600

    setting up user modal and associating links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  Rails.application.routes.draw do
-    root 'links#index'
-    resources :links, only: [:create]
-    get '/:short_url', to: 'links#redirect', as: :short
-  end
+  devise_for :users
+
+  root 'links#index'
+
+  resources :links, only: [:create]
+  get '/:short_url', to: 'links#redirect', as: :short
 end
